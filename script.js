85f40777ed2a41eca49e6a6e7fc902517abf6d5f
ArtGSF2007 <arthur28092007@gmail.com>
2024-06-18 10:20:20 -0300

    Atualização geral

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const foto = document.getElementsByClassName('logoMenor')
+const foto = document.getElementsByClassName('change1')
+foto.addEventsListener('click', trocaFoto)
+let numero = 1
 
-function trocaFoto(novaFoto) {
-    foto.src = 'assets/' + novaFoto
+function trocaFoto() {
+    if (numero == 1) {
+        foto.src = 'assets'
+        numero == 2
+    } else {
+        foto.src = 'assets'
+    }
+
 }
